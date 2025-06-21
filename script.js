27d7d8c5a6d5c31d7c190609fbd3e93b7e31d5df
daniel-rawana <daniel_rawana@outlook.com>
2025-06-21 18:33:03 -0400

    images

--- script.js
+++ script.js
@@ -6,151 +6,172 @@ const locations = [
   title: "10 Toporowski Ave, Richmond Hill, ON",
   lat: 43.8902,
   lng: -79.4326,
-  images: [
-    "images/10Toporowski/1.jpg",
-    "images/10Toporowski/2.jpg",
-    "images/10Toporowski/IMG_1091.jpg",
-    "images/10Toporowski/IMG_1095.jpg",
-    "images/10Toporowski/IMG_1107.jpg",
-    "images/10Toporowski/IMG_1324.jpg",
-    "images/10Toporowski/IMG_1325.jpg",
-    "images/10Toporowski/IMG_1337.jpg"
-  ]
+ images: [
+  "images/10Toporowski/1.jpg",
+  "images/10Toporowski/2.jpg",
+  "images/10Toporowski/IMG_1091.JPG",
+  "images/10Toporowski/IMG_1095.JPEG",
+  "images/10Toporowski/IMG_1107.JPG",
+  "images/10Toporowski/IMG_1324.JPG",
+  "images/10Toporowski/IMG_1325.JPG",
+  "images/10Toporowski/IMG_1337.JPEG"
+]
 },
   { title: "Barrie", lat: 44.3894, lng: -79.6903, images: [
-  "images/Barrie/IMG_1489.jpg",
-  "images/Barrie/IMG_1497.jpg"
+  "images/Barrie/IMG_1489.JPEG",
+  "images/Barrie/IMG_1497.JPEG"
 ]
  },
   { title: "Cancun", lat: 21.1619, lng: -86.8515, images: [
-  "images/Cancun/IMG_1112.jpg",
-  "images/Cancun/IMG_2462.jpg",
-  "images/Cancun/IMG_2465.jpg",
-  "images/Cancun/IMG_2503.jpg"
+  "images/Cancun/IMG_1112.JPG",
+  "images/Cancun/IMG_2462.JPEG",
+  "images/Cancun/IMG_2465.JPEG",
+  "images/Cancun/IMG_2503.JPEG"
 ]
+
 },
   { title: "Downtown Toronto", lat: 43.651070, lng: -79.347015, images: [
-  "images/DowntownToronto/IMG_0106.jpg",
-  "images/DowntownToronto/IMG_0139.jpg",
-  "images/DowntownToronto/IMG_1118.jpg",
-  "images/DowntownToronto/IMG_1637.jpg",
-  "images/DowntownToronto/IMG_1768.jpg",
-  "images/DowntownToronto/IMG_2588.jpg",
-  "images/DowntownToronto/IMG_2898.jpg",
-  "images/DowntownToronto/IMG_4121.jpg",
-  "images/DowntownToronto/IMG_0110.jpg",
-  "images/DowntownToronto/IMG_1106.jpg",
-  "images/DowntownToronto/IMG_1464.jpg",
-  "images/DowntownToronto/IMG_1642.jpg",
-  "images/DowntownToronto/IMG_2446.jpg",
-  "images/DowntownToronto/IMG_2603.jpg",
-  "images/DowntownToronto/IMG_2905.jpg",
-  "images/DowntownToronto/IMG_5136.jpg",
-  "images/DowntownToronto/IMG_0135.jpg",
-  "images/DowntownToronto/IMG_1116.jpg",
-  "images/DowntownToronto/IMG_1468.jpg",
-  "images/DowntownToronto/IMG_1766.jpg",
-  "images/DowntownToronto/IMG_2538.jpg",
-  "images/DowntownToronto/IMG_2893.jpg",
-  "images/DowntownToronto/IMG_4080.jpg",
-  "images/DowntownToronto/IMG_6650.jpg"
+  "images/DowntownToronto/IMG_0106.JPEG",
+  "images/DowntownToronto/IMG_0110.JPEG",
+  "images/DowntownToronto/IMG_0135.JPEG",
+  "images/DowntownToronto/IMG_0139.JPEG",
+  "images/DowntownToronto/IMG_1106.JPG",
+  "images/DowntownToronto/IMG_1116.JPEG",
+  "images/DowntownToronto/IMG_1118.JPG",
+  "images/DowntownToronto/IMG_1464.JPG",
+  "images/DowntownToronto/IMG_1468.JPG",
+  "images/DowntownToronto/IMG_1637.JPEG",
+  "images/DowntownToronto/IMG_1642.JPEG",
+  "images/DowntownToronto/IMG_1766.JPEG",
+  "images/DowntownToronto/IMG_1768.JPEG",
+  "images/DowntownToronto/IMG_2446.JPEG",
+  "images/DowntownToronto/IMG_2538.JPEG",
+  "images/DowntownToronto/IMG_2588.JPEG",
+  "images/DowntownToronto/IMG_2603.JPEG",
+  "images/DowntownToronto/IMG_2893.JPEG",
+  "images/DowntownToronto/IMG_2898.JPEG",
+  "images/DowntownToronto/IMG_4080.JPEG",
+  "images/DowntownToronto/IMG_4121.JPEG",
+  "images/DowntownToronto/IMG_5136.JPEG",
+  "images/DowntownToronto/IMG_6650.JPEG"
 ]
 },
   { title: "Ft. Lauderdale", lat: 26.1224, lng: -80.1373, images: [
   "images/FtLauderdale/1.jpg",
   "images/FtLauderdale/2.jpg",
-  "images/FtLauderdale/IMG_0754.jpg",
-  "images/FtLauderdale/IMG_0770.jpg",
-  "images/FtLauderdale/IMG_0773.jpg",
-  "images/FtLauderdale/IMG_0774.jpg",
-  "images/FtLauderdale/IMG_0775.jpg",
-  "images/FtLauderdale/IMG_6543.jpg",
-  "images/FtLauderdale/IMG_6553.jpg"
+  "images/FtLauderdale/IMG_0754.JPEG",
+  "images/FtLauderdale/IMG_0770.JPEG",
+  "images/FtLauderdale/IMG_0773.JPEG",
+  "images/FtLauderdale/IMG_0774.JPEG",
+  "images/FtLauderdale/IMG_0775.JPEG",
+  "images/FtLauderdale/IMG_6543.JPEG",
+  "images/FtLauderdale/IMG_6553.JPEG"
 ]
  },
   { title: "Grand River", lat: 43.2200, lng: -80.2600, images: [
-  "images/GrandRiver/IMG_1090.jpg",
-  "images/GrandRiver/IMG_1327.jpg",
-  "images/GrandRiver/IMG_2195.jpg"
+  "images/GrandRiver/IMG_1090.JPG",
+  "images/GrandRiver/IMG_1327.JPEG",
+  "images/GrandRiver/IMG_2195.JPEG"
 ]
  }, // Approximate near Brantford/Cambridge
   { title: "Jackson's Point", lat: 44.3492, lng: -79.3689, images: [
-  "images/JacksonsPoint/IMG_1418.jpg",
-  "images/JacksonsPoint/IMG_1422.jpg",
-  "images/JacksonsPoint/IMG_1425.jpg",
-  "images/JacksonsPoint/IMG_1427.jpg",
-  "images/JacksonsPoint/IMG_1447.jpg"
+  "images/JacksonsPoint/IMG_1418.JPEG",
+  "images/JacksonsPoint/IMG_1422.JPEG",
+  "images/JacksonsPoint/IMG_1425.JPEG",
+  "images/JacksonsPoint/IMG_1427.JPEG",
+  "images/JacksonsPoint/IMG_1447.JPEG"
 ]
  },
   { title: "London, ON", lat: 42.9849, lng: -81.2453, images: [
-  "images/London/IMG_0029.jpg",
-  "images/London/IMG_1102.jpg",
-  "images/London/IMG_1142.jpg",
-  "images/London/IMG_1143.jpg",
-  "images/London/IMG_1144.jpg",
-  "images/London/IMG_1145.jpg",
-  "images/London/IMG_1203.jpg",
-  "images/London/IMG_1204.jpg",
-  "images/London/IMG_1232.jpg",
-  "images/London/IMG_1233.jpg",
-  "images/London/IMG_1246.jpg",
-  "images/London/IMG_1247.jpg",
-  "images/London/IMG_1252.jpg",
-  "images/London/IMG_1256.jpg",
-  "images/London/IMG_1257.jpg",
-  "images/London/IMG_1260.jpg",
-  "images/London/IMG_1521.jpg",
-  "images/London/IMG_1525.jpg",
-  "images/London/IMG_2220.jpg",
-  "images/London/IMG_2229.jpg",
-  "images/London/IMG_2233.jpg",
-  "images/London/lp_image.jpg"
+  "images/London/IMG_0029.JPEG",
+  "images/London/IMG_1102.JPG",
+  "images/London/IMG_1142.JPEG",
+  "images/London/IMG_1143.JPEG",
+  "images/London/IMG_1144.JPG",
+  "images/London/IMG_1145.JPG",
+  "images/London/IMG_1204.JPG",
+  "images/London/IMG_1232.JPG",
+  "images/London/IMG_1233.JPG",
+  "images/London/IMG_1246.JPEG",
+  "images/London/IMG_1247.JPEG",
+  "images/London/IMG_1252.JPG",
+  "images/London/IMG_1256.JPG",
+  "images/London/IMG_1257.JPG",
+  "images/London/IMG_1260.JPG",
+  "images/London/IMG_1521.JPEG",
+  "images/London/IMG_1525.JPEG",
+  "images/London/IMG_2220.JPEG",
+  "images/London/IMG_2229.JPEG",
+  "images/London/IMG_2233.JPEG",
+  "images/London/lp_image.JPEG"
 ]
  },
   { title: "Markham", lat: 43.8561, lng: -79.3370, images: [
-  "images/Markham/IMG_0914.jpg",
-  "images/Markham/IMG_1099.jpg",
-  "images/Markham/IMG_1100.jpg",
-  "images/Markham/IMG_1311.jpg",
-  "images/Markham/IMG_2359.jpg",
-  "images/Markham/IMG_2362.jpg",
-  "images/Markham/IMG_2386.jpg",
-  "images/Markham/IMG_2430.jpg",
-  "images/Markham/IMG_2433.jpg",
-  "images/Markham/IMG_2436.jpg",
-  "images/Markham/IMG_2939.jpg"
+  "images/Markham/IMG_0914.JPEG",
+  "images/Markham/IMG_1099.JPG",
+  "images/Markham/IMG_1100.JPG",
+  "images/Markham/IMG_1311.JPG",
+  "images/Markham/IMG_2359.JPEG",
+  "images/Markham/IMG_2362.JPEG",
+  "images/Markham/IMG_2386.JPEG",
+  "images/Markham/IMG_2430.JPEG",
+  "images/Markham/IMG_2433.JPEG",
+  "images/Markham/IMG_2436.JPEG",
+  "images/Markham/IMG_2939.JPEG"
 ]
  },
   { title: "Miami", lat: 25.7617, lng: -80.1918, images: [
   "images/Miami/1.jpg",
-  "images/Miami/IMG_0781.jpg",
-  "images/Miami/IMG_0947.jpg",
-  "images/Miami/IMG_1021.jpg",
-  "images/Miami/IMG_1041.jpg",
-  "images/Miami/IMG_6494.jpg",
-  "images/Miami/IMG_6495.jpg",
-  "images/Miami/IMG_6501.jpg",
-  "images/Miami/IMG_6614.jpg",
-  "images/Miami/IMG_6629.jpg"
+  "images/Miami/IMG_0781.JPEG",
+  "images/Miami/IMG_1021.JPEG",
+  "images/Miami/IMG_1041.JPEG",
+  "images/Miami/IMG_6494.JPEG",
+  "images/Miami/IMG_6495.JPEG",
+  "images/Miami/IMG_6501.JPEG",
+  "images/Miami/IMG_6614.JPEG",
+  "images/Miami/IMG_6629.JPEG"
 ]
  },
   { title: "Montreal", lat: 45.5019, lng: -73.5674, images: [
-  "images/Montreal/IMG_2834.jpg",
-  "images/Montreal/IMG_2836.jpg",
-  "images/Montreal/IMG_2838.jpg",
-  "images/Montreal/IMG_2841.jpg",
-  "images/Montreal/IMG_2843.jpg",
-  "images/Montreal/IMG_2844.jpg",
-  "images/Montreal/IMG_2846.jpg",
-  "images/Montreal/IMG_2852.jpg",
-  "images/Montreal/IMG_2854.jpg",
-  "images/Montreal/IMG_2856.jpg"
+  "images/Montreal/IMG_2834.JPEG",
+  "images/Montreal/IMG_2836.JPEG",
+  "images/Montreal/IMG_2838.JPEG",
+  "images/Montreal/IMG_2841.JPEG",
+  "images/Montreal/IMG_2843.JPEG",
+  "images/Montreal/IMG_2844.JPEG",
+  "images/Montreal/IMG_2846.JPEG",
+  "images/Montreal/IMG_2852.JPEG",
+  "images/Montreal/IMG_2854.JPEG",
+  "images/Montreal/IMG_2856.JPEG"
 ]
 },
-  { title: "North York", lat: 43.7615, lng: -79.4111, images: [] },
-  { title: "Richmond Hill", lat: 43.8828, lng: -79.4403, images: [] },
-  { title: "Schomberg", lat: 44.0042, lng: -79.6842, images: [] },
-  { title: "Toronto Island", lat: 43.6205, lng: -79.3781, images: [] },
+  { title: "North York", lat: 43.7615, lng: -79.4111, images: [
+  "images/NorthYork/1.JPEG",
+  "images/NorthYork/IMG_1475.JPEG",
+  "images/NorthYork/IMG_1481.JPG"
+]
+ },
+  { title: "Richmond Hill", lat: 43.8828, lng: -79.4403, images: [
+  "images/RichmondHill/IMG_1093.JPEG",
+  "images/RichmondHill/IMG_1094.JPEG",
+  "images/RichmondHill/IMG_1289.JPEG",
+  "images/RichmondHill/IMG_1292.JPEG",
+  "images/RichmondHill/IMG_1518.JPEG",
+  "images/RichmondHill/IMG_2408.JPG"
+]
+ },
+  { title: "Schomberg", lat: 44.0042, lng: -79.6842, images: [
+  "images/Schomberg/IMG_1550.JPG",
+  "images/Schomberg/IMG_1566.JPG"
+]
+ },
+  { title: "Toronto Island", lat: 43.6205, lng: -79.3781, images: [
+  "images/TorontoIsland/IMG_0033.JPEG",
+  "images/TorontoIsland/IMG_0036.JPEG",
+  "images/TorontoIsland/IMG_0054.JPEG",
+  "images/TorontoIsland/IMG_0055.JPEG"
+]
+ },
   { title: "LA (Future)", lat: 34.0522, lng: -118.2437, images: [], message: "To be written..." }
 ];
 
